c5964aca4c294def4b72ae4e1b9947561dd0328a
Kylian RAULIN <raulin.kylian@gmail.com>
2023-01-21 19:09:02 +0100

    récupération données api & début affichage

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+
+<script >
+import { resolveComponentType } from '@vue/compiler-core';
+import { def } from '@vue/shared';
+import axios from 'axios';
+
+  export default {
+    name:'App',
+    data(){
+      return{
+        films: null,
+      }
+    },
+    mounted(){
+      axios
+      .get('https://api.themoviedb.org/3/movie/now_playing?api_key=7a4c83706af8cbec688d9366d2f9ecd2&language=fr')
+      .then((reponse) => {
+        this.films = reponse.data.results;
+        console.log(this.films)
+      });
+    },
+  }
+
+</script>
+
+
+<template>
+<header>
+  
+  <section class="banner">
+    <div id="logo">
+      <img src="./assets/Movies_Moment.png" alt="logo">
+    </div>
+    <div class="title">
+      <h1>Les films du moment</h1>
+    </div>
+  </section>
+</header>
+<body>
+  <section class="Filter">
+    <h1 class="title-filter">filtres</h1>
+      <div class="filtered-buttons">
+       <button class="Featured" v-on:click="films_vedette">En vedette</button>
+        <button class="top-rated" v-on:click="Les-mieux-notés">Les mieux notés</button>
+      </div>
+  </section>
+  <div :key="index" v-for="(film, index) in films">
+        <img :src="'https://image.tmdb.org/t/p/w500' + film.poster_path">
+        <h1>{{ film.title }}</h1>
+    </div>
+</body>
+<footer>
+    <div class="logo_footer">
+      <img src="./assets/Movies_Moment.jpg" alt="logo">
+    </div>
+</footer>
+  <main>
+
+  </main>
+
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles/Filter.css'
+import './assets/main.css'
+import './styles/Banner.css'
+import './styles/footer.css'
+
+
+createApp(App).mount('#app')
